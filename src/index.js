57d70a5da763880f7d8d58eb33c9008bc69e076e
vcordero07 <vcordero07@gmail.com>
2018-01-29 20:00:56 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import './index.css';
 
 import App from './components/game';
 
-ReactDOM.render(<App />,
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
